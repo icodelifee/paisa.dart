1b956b67dc002522c89d65f2f40ac36973f39083
icodelifee <mithunsmenon@gmail.com>
2022-10-28 02:33:45 +0530

    doc: add documentation for the code

--- lib/src/currency_convertor.dart
+++ lib/src/currency_convertor.dart
@@ -12,29 +12,49 @@ class CurrencyConvertor {
 
   CurrencyConvertor._internal();
 
+  /// Converts the given [amount] from one currency to another.
+  ///
+  ///  * [from] is the currency enum to convert from.
+  /// * [to] is the currency enum to convert to.
+  /// * [amount] is the amount to convert.
+  ///
+  /// [Currency] enum is used to specify the currency.<br>
+  /// Returns the converted amount.
+  ///
+  /// Throws [ArgumentError] if [amount] is null or less than 0.<br>
+  /// Throws [ArgumentError] if [from] and [to] are the same.
   static Future<double> convert({required Currency from, required Currency to, required double amount}) async {
-    assert(from != to, 'From and To currencies cannot be the same');
-    assert(amount > 0, 'Amount must be greater than 0');
+    if (from == to) throw ArgumentError('from and to currencies cannot be the same');
+    if (amount <= 0) throw ArgumentError('amount cannot be less than 0');
 
     return _getCurrencyConversion(from.name, to.name, amount);
   }
 
+  /// Converts the [amount] from the [from] currency to the [to] currency. <br>
+  /// [convertFromString] accepts [String] currency codes eg: USD, INR, EUR etc <br>
+  /// eg: ```convertFromString('USD', 'INR', 100)``` will convert 100 USD to INR
   static Future<double> convertFromString({required String from, required String to, required double amount}) async {
-    assert(from.toLowerCase() != to.toLowerCase(), 'From and To currencies cannot be the same');
-    assert(amount > 0, 'Amount must be greater than 0');
+    if (from == to) throw ArgumentError('from and to currencies cannot be the same');
+    if (amount <= 0) throw ArgumentError('amount cannot be less than 0');
 
     return _getCurrencyConversion(from, to, amount);
   }
 
+  /// Return the currency conversion rate for the given [from] and [to] currencies<br>
+  /// eg: ```rate(Currency.USD, Currency.INR) => 73.5```
   static Future<double> rate(Currency from, Currency to) async {
     return convert(from: from, to: to, amount: 1);
   }
 
+  /// Returns the currency conversion rate for the given [from] and [to] currencies strings<br>
+  /// eg: rateFromString('USD', 'INR') returns the conversion rate for USD to INR
   static Future<double> rateFromString(String from, String to) async {
     return convertFromString(from: from, to: to, amount: 1);
   }
 
-  static Future rateFromCountryCode({required String from, required String to}) async {
+  /// Returns the currency conversion rate for the given [from] and [to] country codes.
+  /// <br>eg.  ```CurrencyConvertor.rateFromCountryCode(from:'US', to:'IN');```
+  static Future<double> rateFromCountryCode({required String from, required String to}) async {
     final ccFrom = CountryCode.values.fromCode(from);
     final ccTo = CountryCode.values.fromCode(to);
     if (ccFrom == null || ccTo == null) {
