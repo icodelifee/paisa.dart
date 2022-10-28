fe39c86d44273dd5af019327c58ee5dceda611ac
icodelifee <mithunsmenon@gmail.com>
2022-10-28 18:53:45 +0530

    feat: implement paisa model

---
+++ lib/src/entities/models/paisa.dart
@@ -0,0 +1,25 @@
+import 'package:paisa/paisa.dart';
+
+class Paisa {
+  const Paisa({
+    required this.from,
+    required this.to,
+    required this.amount,
+    required this.rate,
+  });
+
+  final Currency from;
+  final Currency to;
+  final double amount;
+  final double rate;
+
+  double get convertedAmount => amount * rate;
+
+  String get toCurrencySymbol => to.currencySymbol;
+  String get fromCurrencySymbol => from.currencySymbol;
+
+  @override
+  String toString() {
+    return 'Paisa(from: $from, to: $to, amount: $amount, rate: $rate)';
+  }
+}
